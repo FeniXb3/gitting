6c6758f974658249a0e091719939036f5e1cd385
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-09-26 11:58:36 +0200

    Improve casear decrypt

--- ceasar.py
+++ ceasar.py
@@ -14,9 +14,25 @@ def encrypt(text, s):
 # check the above function
 
 
+def decrypt(text, s):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        # Encrypt uppercase characters in plain text
+
+        if (char.isupper()):
+            result += chr((ord(char) - s - 65) % 26 + 65)
+        # Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(char) - s - 97) % 26 + 97)
+    return result
+
+
 text = "CEASER CIPHER DEMO"
 s = 4
 
 print("Plain Text : " + text)
 print("Shift pattern : " + str(s))
 print("Cipher: " + encrypt(text, s))
+
+print("Decipher: " + decrypt(encrypt(text, s), s))
